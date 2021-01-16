6790ac39493e1455e4d99471b32654771a61031b
Eray Kayan <eray.kayan@campus.netkent.edu.tr>
2021-01-16 23:51:09 +0300

    C#

---
+++ KampIntro/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace KampIntro
+{
+	class Program
+	{
+		private const string Value = "";
+
+		static void Main(string[] args)
+		{
+			// type safety = tip guvenligi
+			// Do not repeat yourself
+			string kategoriEtiketi = "Kategoriler";
+			int ogrenciSayisi = 32000;
+			double faizOrani = 1.45;
+			bool sistemeGirisYapmisMi = true;
+			double dolarDun = 7.35;
+			double dolarBugun = 7.35;
+
+			if (dolarDun>dolarBugun)
+			{
+				Console.WriteLine("Azalis Butonu");
+
+			}
+
+			else if (dolarDun < dolarBugun)
+			{
+				Console.WriteLine("Artis Butonu");
+
+			}
+
+			else 
+			{
+				Console.WriteLine("Degismedi");
+
+			}
+
+			if (sistemeGirisYapmisMi == true)
+			{
+				Console.WriteLine("Kullanici ayarlari butonu");
+			}
+
+			else 
+			{
+				Console.WriteLine("Giris yap butonu");
+			}
+
+									
+		}
+	}
+}
+ 
---
+++ Metotlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Metotlar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Urun urun1 = new Urun();
+			urun1.Adi = "Elma";
+			urun1.Fiyati = 15;
+			urun1.Aciklama = "Amasya elmasi";
+
+			Urun urun2 = new Urun();
+			urun2.Adi = "Karpuz";
+			urun2.Fiyati = 80;
+			urun2.Aciklama = "Diyarbakir Karpuzu";
+
+			Urun[] urunler = new Urun[] {urun1,urun2 };
+
+			//type-safe
+			foreach (Urun urun in urunler)
+			{
+				Console.WriteLine(urun.Adi);
+				Console.WriteLine(urun.Fiyati);
+				Console.WriteLine(urun.Aciklama);
+				Console.WriteLine("--------------------");
+			}
+
+			Console.WriteLine("--------METOTLAR--------");
+
+			SepetManager sepetManager = new SepetManager();
+			sepetManager.Ekle(urun1);
+			sepetManager.Ekle(urun2);
+
+
+			sepetManager.Ekle2("Armut", "Yesil Armut", 12, 8);
+			sepetManager.Ekle2("Elma", "Yesil Elma", 12, 8) ;
+			sepetManager.Ekle2("Karpuz", "Kirmizi Armut", 12, 6) ;
+		}
+	}
+}
+
+
+
